c1cc75013fcbbf6193d8493f9d9039753dd1d1dc
Santiago <new.noguera@gmail.com>
2025-02-19 11:43:01 -0500

    mas salidas por consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
+
         system.out.println("ola");
 
         system.out.println("Algo nuevo");
+
+        system.out.println("Otra salida");
+
     }
 }
